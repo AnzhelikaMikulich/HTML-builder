626494b4151e49253b1cc80198d1adc3f690f4dd
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-11-08 05:04:52 +0300

    feat:add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const path = require("path");
+const fs = require("fs");
+const promis = require("fs/promises");
+const folderName = path.join(__dirname, "files");
+const folderNameCopy = path.join(__dirname, "files-copy"); //путь для проверки папки
 
+async function addFolder() {
+  await promis.rm(folderNameCopy, { recursive: true, force: true }); //delete folder
+  await promis.mkdir(folderNameCopy, { recursive: true }); //add folder
+}
+
+async function copyFolder() {
+  promis.readdir(folderName, { withFileTypes: true }).then((result) =>
+    result.forEach((element) => {
+      promis.copyFile(
+        path.join(folderName, element.name),
+        path.join(folderNameCopy, element.name)
+      ); //copy files
+    })
+  );
+}
+
+async function runCopy() {
+  await addFolder();
+  await copyFolder();
+}
+runCopy();
